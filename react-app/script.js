823b8826cddd261a5ad588ccfebb8c00c694a657
saheelwagh <saheelwagh@gmail.com>
2024-01-22 22:51:56 +0530

    fcc code misc

---
+++ react-app/script.js
@@ -0,0 +1,18 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let monsterHealth;
+let inventory = ["stick", "dagger", "sword"];
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+
+// JavaScript interacts with the HTML using the Document Object Model,
+//  or DOM. The DOM is a tree of objects that represents the HTML
+//  You can access the HTML using the document object, which represents your entire HTML document.
+// One method for finding specific elements in your HTML is using the querySelector() method. The querySelector() method takes a CSS selector as an argument and returns the first element that matches that selector.
+
